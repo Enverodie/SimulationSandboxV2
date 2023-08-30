f1887b6f9794d0e5ab5cd6b3b77c112d37f62b7d
sms10 <sms10@iu.edu>
2023-08-30 16:13:30 -0400

    Coded some ideas for render styles model

--- src/model/globalVars.ts
+++ src/model/globalVars.ts
@@ -13,3 +13,18 @@ export const DEFAULT_TIME_TO_SPROUT = 1;
 // TODO: implement this boundary
 export const MAX_TILES_SPREADCONDITION = 8+18;
 export const MAX_TILES_SPREADSTRATEGY = 32;
+
+export const DEFAULT_COLOR_LOOP_TIME = TICKS_BETWEEN_GENERATIONS;
+export const DEFAULT_COLOR_GRADIENT = [
+    {r: 255, g: 0, b: 0},
+    {r: 255, g: 154, b: 0},
+    {r: 208, g: 222, b: 33},
+    {r: 79, g: 220, b: 74},
+    {r: 63, g: 218, b: 216},
+    {r: 47, g: 201, b: 226},
+    {r: 28, g: 127, b: 238},
+    {r: 95, g: 21, b: 242},
+    {r: 186, g: 12, b: 248},
+    {r: 251, g: 7, b: 217},
+    {r: 255, g: 0, b: 0},
+];
--- src/model/renderStyles.ts
+++ src/model/renderStyles.ts
@@ -3,3 +3,42 @@
  * throughout simulation sandbox -
  * Colors, gradients, spectrum cycling, etc.
  */
+
+import { DEFAULT_COLOR_LOOP_TIME, DEFAULT_COLOR_GRADIENT } from "./globalVars";
+
+export type RGB = { r: number, g: number, b: number };
+export type RGBA = { r: number, g: number, b: number, a: number };
+
+type RenderColorFunctions_Options = {
+    loopAfterTicks?: number,
+    color?: RGB,
+    colorGradient?: RGB[],
+}
+
+export const Cell_RenderColorFunctions:{[key:string]: (tick:number, options:RenderColorFunctions_Options) => RGB} = {
+    solid: (tick, options) => {
+        if (!options || !options.color) return {r: 0, g: 0, b: 0};
+        return options.color;
+    },
+
+    spectrumCycling: (tick, options) => {
+        const loopAfterTicks = options.loopAfterTicks || DEFAULT_COLOR_LOOP_TIME;
+        const advanceRate = 1/loopAfterTicks;
+        let advancedAmount = (tick%loopAfterTicks) * advanceRate;
+        const colorGradient = options.colorGradient || DEFAULT_COLOR_GRADIENT;
+        let color1Index = Math.floor(advancedAmount * colorGradient.length), color2Percentage = (advancedAmount * colorGradient.length) - color1Index;
+        let color2Index = color1Index + 1, color1Percentage = 1 - color2Percentage;
+        let color1 = colorGradient[color1Index];
+        let color2 = colorGradient[color2Index];
+        return {
+            r: (color1.r * color1Percentage) + (color2.r * color2Percentage),
+            g: (color1.g * color1Percentage) + (color2.g * color2Percentage),
+            b: (color1.b * color1Percentage) + (color2.b * color2Percentage),
+        }
+    }
+
+}
+
+class CellRenderStrategy {
+
+}
